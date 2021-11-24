10ab0d4773958939b8c90b24b3abd080a19eedf6
IvanMatsyuk <94521867+IvanMatsyuk@users.noreply.github.com>
2021-11-24 21:29:08 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main(){
     
-    int A[12];
+    int A[12],k;
     float C[12];
     srand(time(NULL));
     printf("Сгенерований масив А\n");
@@ -13,11 +13,21 @@ int main(){
         A[i]=100-rand()%200;
         printf("A[%i]=%i\n",i, A[i]);
     }
+    printf("Введіть знначення k = ");
+    scanf("%i",&k);
+         if(k==0){
+             printf("На нуль ділити не можна");
+             exit(0);
+         }
      printf("Сгенерований масив C\n");
-     for(int i=0; i<12; i++){
-        C[i]=A[i]%3;
-        
+      for(int i=0; i<12; i++){
+         if (k<10){
+        C[i]=A[i]/k;
         printf("C[%i]=%5.2f\n",i, C[i]);
+         }
+     
      }
+     
+     
     return 0;
 }
