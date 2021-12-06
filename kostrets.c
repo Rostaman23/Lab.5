e9e2d782334b164aad7596ebc55f3072d75223e0
VitaliiKostrets <94521789+VitaliiKostrets@users.noreply.github.com>
2021-12-06 10:45:49 +0200

    Update kostrets.c

--- kostrets.c
+++ kostrets.c
@@ -5,30 +5,40 @@
 
 int main(){
     
-    int A[12], i, n;
-    float C[12], dob;
+    int A[12], i, n, k, dob1, dob2;
+    float C[12];
     srand(time(NULL));
     printf("Сгенерований масив А\n");
     for(int i=0; i<12; i++){
         A[i]=100-rand()%200;
         printf("A[%i]=%i\n",i, A[i]);
     }
+    printf("Введіть знначення k = ");
+    scanf("%i",&k);
+         if(k==0){
+             printf("На нуль ділити не можна");
+             exit(0);
+         }
      printf("Сгенерований масив C\n");
-     for(int i=0; i<12; i++){
-        C[i]=A[i]%3;
-        
+      for(int i=0; i<12; i++){
+         if (k<10){
+        C[i]=A[i]/k;
         printf("C[%i]=%5.2f\n",i, C[i]);
-     }
-     dob=1;
+         }
+          
+      }
+      
+     dob1=1;
      i=0;
       while(i<11){
-          if(C[i]>0.00 || i<11){
-          dob=C[i]*C[i];
+          if(C[i]>0 || i<=11){
+          dob1=C[i];
+          dob2=dob1*dob1;
           i++;
           
           }
       }
-      printf("dob=%f\n", dob);
+      printf("dob=%i\n", dob2);
      
     return 0;
 }
